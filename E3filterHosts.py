9b442950e8af551f6ed57d2c4af7eb51bdd2e153
aexp-tushar <tushar.bhatia2@aexp.com>
2024-12-10 17:07:47 +0530

    Changes for grep

--- E3filterHosts.py
+++ E3filterHosts.py
@@ -54,61 +54,61 @@ for service in hosts.keys():
             if service == "CDaaS":
                 print("Validating ",service," hosts")
                 os.system('''
-                          sshpass -p%(passwd)s  ansible all -i E3_host_CDaaS -u  %(ads)s -m shell -a "echo $'\nNginx status\n' && systemctl status nginx | grep 'Active' ;  echo $'\nmnt-cdaas.mount status\n'  &&  systemctl status mnt-cdaas.mount | grep 'Active'  ; echo $'\nmnt-cdaas.automount status\n' &&  systemctl status mnt-cdaas.automount | grep 'Active'  ; echo $'\nnode-exporter status\n' && systemctl status node-exporter | grep 'Active'  ; echo $'\nsplunk status\n'  &&  systemctl status splunk | grep 'Active'  ; echo $'\necp status\n' && service ecp status | grep 'Active'  ; echo $'\nDeployer  status\n'  &&  systemctl status deployer | grep 'Active'" -bk
+                          sshpass -p%(passwd)s  ansible all -i E3_host_CDaaS -u  %(ads)s -m shell -a "echo $'\nNginx status\n' && systemctl status nginx | grep -i 'Active' ;  echo $'\nmnt-cdaas.mount status\n'  &&  systemctl status mnt-cdaas.mount | grep -i 'Active'  ; echo $'\nmnt-cdaas.automount status\n' &&  systemctl status mnt-cdaas.automount | grep -i 'Active'  ; echo $'\nnode-exporter status\n' && systemctl status node-exporter | grep -i 'Active'  ; echo $'\nsplunk status\n'  &&  systemctl status splunk | grep -i 'Active'  ; echo $'\necp status\n' && service ecp status | grep -i 'Active'  ; echo $'\nDeployer  status\n'  &&  systemctl status deployer | grep -i 'Active'" -bk
                           ''' % locals())
             if service == "ESaaS":
                 print("Validating ",service," hosts")
                 os.system('''
-                          sshpass -p%(passwd)s ansible all -i E3_host_ESaaS -u %(ads)s -m shell -a "systemctl status elasticsearch | grep 'Active'" -bk
+                          sshpass -p%(passwd)s ansible all -i E3_host_ESaaS -u %(ads)s -m shell -a "systemctl status elasticsearch | grep -i 'Active'" -bk
                           ''' % locals())
             if service == "AeroCache":
                print("Validating ",service," hosts")
                os.system('''
-                        sshpass -p%(passwd)s ansible all -i E3_host_AeroCache -u %(ads)s -m shell -a "systemctl status aerospike | grep 'Active'" -bk
+                        sshpass -p%(passwd)s ansible all -i E3_host_AeroCache -u %(ads)s -m shell -a "systemctl status aerospike | grep -i 'Active'" -bk
                          ''' % locals())
             if service in ["Kafka","kafka"]:
                 print("Validating ",service," hosts")
                 if kafka:
                     print("Running kafka role validation")
                     os.system('''
-                          sshpass -p%(passwd)s ansible all -i E3_host_kafka -u %(ads)s -m shell -a "systemctl status kafka | grep 'Active'" -bk
+                          sshpass -p%(passwd)s ansible all -i E3_host_kafka -u %(ads)s -m shell -a "systemctl status kafka | grep -i 'Active'" -bk
                           ''' % locals())
                 if ccc:
                     print("Running ccc role validation")
                     os.system('''
-                            sshpass -p%(passwd)s ansible all -i E3_host_kafka -u %(ads)s -m shell -a "systemctl status ccc | grep 'Active'" -bk
+                            sshpass -p%(passwd)s ansible all -i E3_host_kafka -u %(ads)s -m shell -a "systemctl status ccc | grep -i 'Active'" -bk
                               ''' % locals())
                 if schreg:
                     print("Running SchemaRegistry role validation")    
                     os.system('''
-                            sshpass -p%(passwd)s ansible all -i E3_host_kafka -u %(ads)s -m shell -a "systemctl status schemaregistry | grep 'Active'" -bk
+                            sshpass -p%(passwd)s ansible all -i E3_host_kafka -u %(ads)s -m shell -a "systemctl status schemaregistry | grep -i 'Active'" -bk
                               ''' % locals())
                 if ksql:
                     print("Running ksql role validation")  
                     os.system('''
-                            sshpass -p%(passwd)s ansible all -i E3_host_kafka -u %(ads)s -m shell -a "systemctl status ksql | grep 'Active'" -bk
+                            sshpass -p%(passwd)s ansible all -i E3_host_kafka -u %(ads)s -m shell -a "systemctl status ksql | grep -i 'Active'" -bk
                               ''' % locals())
             if service == 'Gridgain':
                 print("Validating ",service," hosts")
                 if asm:
                     print("Running Gridgain ASM validation")
                     os.system('''
-                          sshpass -p%(passwd)s ansible all -i  E3_host_Gridgain -u %(ads)s  -m shell -a "echo $'\nWebconsole status\n' && systemctl status webconsole | grep 'Active'; echo $'\nGridgain-risk status\n' && systemctl status gridgain-risk | grep 'Active';echo $'\nGridgain-ram status\n' && systemctl status gridgain-ram | grep 'Active'" -bk
+                          sshpass -p%(passwd)s ansible all -i  E3_host_Gridgain -u %(ads)s  -m shell -a "echo $'\nWebconsole status\n' && systemctl status webconsole | grep -i 'Active'; echo $'\nGridgain-risk status\n' && systemctl status gridgain-risk | grep -i 'Active';echo $'\nGridgain-ram status\n' && systemctl status gridgain-ram | grep -i 'Active'" -bk
                           ''' % locals())
                 if gmnt:
                     print("Running Gridgain GMNT validation")
                     os.system('''
-                            sshpass -p%(passwd)s ansible all -i  E3_host_Gridgain -u %(ads)s  -m shell -a "echo $'\nGridgain-cluster1.service status\n' && systemctl status gridgain-cluster1.service | grep 'Active'; echo $'\nGridgain-cluster2.service status\n' && systemctl status gridgain-cluster2.service | grep 'Active';echo $'\nWebconsole status\n' && systemctl status webconsole | grep 'Active'; echo $'\nWebagent status\n' && systemctl status webagent | grep 'Active';echo $'\nGridgain-cluster3.service status\n' && systemctl status gridgain-cluster3.service | grep 'Active';echo $'\nGridgain-cluster4.service status\n' && systemctl status gridgain-cluster4.service | grep 'Active'" -bk
+                            sshpass -p%(passwd)s ansible all -i  E3_host_Gridgain -u %(ads)s  -m shell -a "echo $'\nGridgain-cluster1.service status\n' && systemctl status gridgain-cluster1.service | grep -i 'Active'; echo $'\nGridgain-cluster2.service status\n' && systemctl status gridgain-cluster2.service | grep -i 'Active';echo $'\nWebconsole status\n' && systemctl status webconsole | grep -i 'Active'; echo $'\nWebagent status\n' && systemctl status webagent | grep -i 'Active';echo $'\nGridgain-cluster3.service status\n' && systemctl status gridgain-cluster3.service | grep -i 'Active';echo $'\nGridgain-cluster4.service status\n' && systemctl status gridgain-cluster4.service | grep -i 'Active'" -bk
                               ''' % locals())
                 if abs:
                     print("Running Gridgain ABS validation")    
                     os.system('''
-                            sshpass -p%(passwd)s ansible all -i  E3_host_Gridgain -u %(ads)s  -m shell -a "systemctl status gridgain | grep 'Active'" -bk
+                            sshpass -p%(passwd)s ansible all -i  E3_host_Gridgain -u %(ads)s  -m shell -a "systemctl status gridgain | grep -i 'Active'" -bk
                               ''' % locals())
                 if rc:
                     print("Running Gridgain RC validation")  
                     os.system('''
-                            sshpass -p%(passwd)s ansible all -i  E3_host_Gridgain -u %(ads)s  -m shell -a "echo $'\nGridgain-compute.service status\n' && systemctl status gridgain-compute.service | grep 'Active'; echo $'\nGridgain-client.service status\n' && systemctl status gridgain-client.service | grep 'Active';echo $'\nOneagent.service status\n' && systemctl status oneagent.service | grep 'Active'" -bk
+                            sshpass -p%(passwd)s ansible all -i  E3_host_Gridgain -u %(ads)s  -m shell -a "echo $'\nGridgain-compute.service status\n' && systemctl status gridgain-compute.service | grep -i 'Active'; echo $'\nGridgain-client.service status\n' && systemctl status gridgain-client.service | grep -i 'Active';echo $'\nOneagent.service status\n' && systemctl status oneagent.service | grep -i 'Active'" -bk
                               ''' % locals())
 print("Validation Done ..... Deleting host files.......")
 os.system('''
